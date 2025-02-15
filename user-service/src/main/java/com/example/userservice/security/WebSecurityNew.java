27b12c7abdba03189b5a34097a206519d5959164
j3261221 <j3261221@gmail.com>
2025-02-15 19:59:16 +0900

    [refactor] k8s 및 cicd 적용을 위한 리팩터링 [본문] k8s 적용을 위한 Dockerfile 수정과 Jenkinsfile 추가

--- user-service/src/main/java/com/example/userservice/security/WebSecurityNew.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurityNew.java
@@ -60,7 +60,7 @@ public class WebSecurityNew {
                         .requestMatchers(new AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
 //                        .requestMatchers("/**").access(this::hasIpAddress)
                         .requestMatchers("/**").access(
-                                new WebExpressionAuthorizationManager("hasIpAddress('127.0.0.1') or hasIpAddress('192.168.0.2')")) // host pc ip address
+                                new WebExpressionAuthorizationManager("hasIpAddress('127.0.0.1') or hasIpAddress('192.168.65.3')"))
                         .anyRequest().authenticated()
                 )
                 .authenticationManager(authenticationManager)
